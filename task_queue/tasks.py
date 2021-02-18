4757564b819f6c2b1d6b38fc8f60955daedf5bb9
Bachibouzouk <pierre-francois.duc@rl-institute.de>
2021-02-18 18:44:35 +0100

    Make a deepcopy of input dict

--- task_queue/tasks.py
+++ task_queue/tasks.py
@@ -2,6 +2,7 @@ import os
 import time
 import traceback
 import json
+from copy import deepcopy
 from celery import Celery
 
 from multi_vector_simulator.server import run_simulation as mvs_simulation
@@ -19,20 +20,15 @@ celery = Celery("tasks", broker=CELERY_BROKER_URL, backend=CELERY_RESULT_BACKEND
 def run_simulation(
     simulation_input: dict,
 ) -> dict:
-
+    epa_json = deepcopy(simulation_input)
     dict_values = None
     try:
-
         dict_values = convert_epa_params_to_mvs(simulation_input)
-
         simulation_output = mvs_simulation(dict_values)
-
     except Exception as e:
-
         simulation_output = dict(
             ERROR="{}".format(traceback.format_exc()),
             INPUT_JSON_EPA=simulation_input,
             INPUT_JSON_MVS=dict_values,
         )
-
     return json.dumps(simulation_output)
