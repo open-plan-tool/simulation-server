a9340162efbce4a09857c8ac6ad52da9356f7e8b
pierre-francois.duc <pierre-francois.duc@rl-institut.de>
2024-01-19 15:14:55 +0100

    Increase worker logging

--- task_queue/tasks.py
+++ task_queue/tasks.py
@@ -4,10 +4,13 @@ import traceback
 import json
 from copy import deepcopy
 from celery import Celery
+from celery.utils.log import get_task_logger
 
 from multi_vector_simulator.server import run_simulation as mvs_simulation
 from multi_vector_simulator.utils.data_parser import convert_epa_params_to_mvs
 
+
+logger = get_task_logger(__name__)
 CELERY_BROKER_URL = (os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379"),)
 CELERY_RESULT_BACKEND = os.environ.get(
     "CELERY_RESULT_BACKEND", "redis://localhost:6379"
@@ -20,13 +23,21 @@ app = Celery(CELERY_TASK_NAME, broker=CELERY_BROKER_URL, backend=CELERY_RESULT_B
 
 @app.task(name=f"{CELERY_TASK_NAME}.run_simulation")
 def run_simulation(simulation_input: dict,) -> dict:
+    logger.info("Start new simulation")
     epa_json = deepcopy(simulation_input)
     dict_values = None
     try:
         dict_values = convert_epa_params_to_mvs(simulation_input)
+        logger.debug("Converted epa parameters to mvs input")
         simulation_output = mvs_simulation(dict_values)
+        logger.info("Simulation finished")
         simulation_output["SERVER"] = CELERY_TASK_NAME
     except Exception as e:
+        logger.error(
+            "An exception occured in the simulation task: {}".format(
+                traceback.format_exc()
+            )
+        )
         simulation_output = dict(
             SERVER=CELERY_TASK_NAME,
             ERROR="{}".format(traceback.format_exc()),
